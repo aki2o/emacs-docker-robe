f716cde89dd8af96a5d206fff849c19fa6920822
Otsu.Hiroaki <otsu.hiroaki@moneyforward.co.jp>
2017-05-21 10:33:10 +0900

    cache docker configuration per project

--- docker-robe.el
+++ docker-robe.el
@@ -5,7 +5,7 @@
 ;; Author: Hiroaki Otsu <ootsuhiroaki@gmail.com>
 ;; Keywords: ruby convenience docker
 ;; URL: https://github.com/aki2o/emacs-docker-robe
-;; Version: 0.0.1
+;; Version: 0.1.0
 ;; Package-Requires: ((inf-ruby "2.5.0") (robe "0.7.9"))
 
 ;; This program is free software; you can redistribute it and/or modify
@@ -83,76 +83,189 @@
 (eval-when-compile (require 'cl))
 (require 'inf-ruby)
 (require 'robe)
+(require 'projectile nil t)
 (require 'advice)
 
+(defgroup docker-robe nil
+  "Let robe work under docker container."
+  :group 'convenience
+  :prefix "docker-robe:")
 
-(defvar docker-robe:container nil)
+(defcustom docker-robe:project-cache-file (concat user-emacs-directory ".docker-robe-project")
+  "Filepath stores project configuration."
+  :type 'string
+  :group 'docker-robe)
+
+(defcustom docker-robe:project-root-detect-function 'projectile-project-root
+  "Function detect project root path for `current-buffer'."
+  :type 'symbol
+  :group 'docker-robe)
+
+
 (defvar docker-robe:enabled nil)
-(defvar docker-robe:source-map nil)
+(defvar docker-robe:project-root nil)
+(defvar docker-robe:project-cache-hash nil)
+(defvar docker-robe:container nil)
+(defvar docker-robe:volume-local-path-alist nil)
 
+
+;;;;;;;;;;;;;
+;; Utility
 
 (defun docker-robe::exec (func cmd)
   (if (not (executable-find "docker"))
       (error "Not found 'docker' command. You sure docker has been installed.")
     (funcall func (format "docker %s" cmd))))
 
-(defun docker-robe::select-container ()
-  (let* ((cmd "ps --format='{{.Names}} {{.ID}}'")
-         (container-id-of (loop for line in (split-string (docker-robe::exec 'shell-command-to-string cmd) "\n")
-                                if (> (length line) 1)
-                                collect (apply 'cons (split-string line " "))))
-         (container-names (mapc 'car container-id-of))
-         (container-name (completing-read "select container: " container-names nil t nil '())))
-    (assoc-default container-name container-id-of)))
+(defun docker-robe::container-name (container)
+  (let ((cmd (format "ps --format='{{.Names}}' -f 'id=%s'" container)))
+    (replace-regexp-in-string "\n" "" (docker-robe::exec 'shell-command-to-string cmd))))
 
-(defun docker-robe::select-port (container)
-  (let* ((cmd (format "inspect --format='{{range $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}} {{end}}' %s" container))
-         (ports (split-string (docker-robe::exec 'shell-command-to-string cmd) " ")))
-    (cond
-     ((= (length ports) 0)
-      (error "Not any ports bound to %s" container))
-     ((= (length ports) 1)
-      (nth 0 ports))
-     ((> (length ports) 1)
-      (completing-read "select port: " ports nil t nil '())))))
+(defun docker-robe::container-id (container-name)
+  (let ((cmd (format "ps --format='{{.ID}}' -f 'name=%s'" container-name)))
+    (replace-regexp-in-string "\n" "" (docker-robe::exec 'shell-command-to-string cmd))))
 
-(defun docker-robe::source-map (container)
-  (let* ((cmd (format "inspect --format='{{range $conf := .Mounts}}{{$conf.Destination}}:{{end}}' %s" container)))
-    (loop for remote-path in (split-string (docker-robe::exec 'shell-command-to-string cmd) ":")
-          for local-path = (when (and (string-match "/" remote-path)
-                                      (y-or-n-p (format "Local source is mounted on %s" remote-path)))
-                             (directory-file-name (read-directory-name "Local source path : " nil nil t default-directory)))
-          if local-path
-          collect (cons remote-path local-path))))
+
+;;;;;;;;;;;
+;; Cache
+
+(defun docker-robe::project-cached-value (cache-name)
+  (when docker-robe:project-root
+    (plist-get (docker-robe::ensure-project-cache)
+               cache-name)))
+
+(defun docker-robe::ensure-project-cache ()
+  (gethash docker-robe:project-root
+           (or docker-robe:project-cache-hash
+               (setq docker-robe:project-cache-hash
+                     (or (docker-robe::load-project-cache-hash)
+                         (make-hash-table :test 'equal))))))
+
+(defun docker-robe::load-project-cache-hash ()
+  (when (file-exists-p docker-robe:project-cache-file)
+    (read (with-temp-buffer
+            (insert-file-contents docker-robe:project-cache-file)
+            (buffer-string)))))
+
+(defun* docker-robe::store-project-cache (&key container port volume-local-path-alist)
+  (when docker-robe:project-root
+    (puthash docker-robe:project-root
+             `(:container ,container :port ,port :volume-local-path-alist ,volume-local-path-alist)
+             docker-robe:project-cache-hash)
+    (with-temp-buffer
+      (insert (prin1-to-string docker-robe:project-cache-hash))
+      (write-file docker-robe:project-cache-file))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Config Docker Environment
+
+(defun* docker-robe::select-container (&key (use-cache nil))
+  (or (when use-cache (docker-robe::project-cached-value :container))
+      (let* ((cmd "ps --format='{{.Names}} {{.ID}}'")
+             (res (docker-robe::exec 'shell-command-to-string cmd))
+             (container-id-alist (loop for line in (split-string res "\n")
+                                       if (> (length line) 1)
+                                       collect (apply 'cons (split-string line " "))))
+             (container-names (mapc 'car container-id-alist))
+             (container-name (completing-read "select container: " container-names nil t nil '())))
+        (assoc-default container-name container-id-alist))))
+
+(defun* docker-robe::select-port (container &key (use-cache nil))
+  (or (when use-cache (docker-robe::project-cached-value :port))
+      (let* ((cmd (format "inspect --format='{{range $conf := .NetworkSettings.Ports}}{{(index $conf 0).HostPort}} {{end}}' %s" container))
+             (ports (split-string (docker-robe::exec 'shell-command-to-string cmd) " ")))
+        (cond
+         ((= (length ports) 0)
+          (error "Not any ports bound to %s" container))
+         ((= (length ports) 1)
+          (nth 0 ports))
+         ((> (length ports) 1)
+          (completing-read "select port: " ports nil t nil '()))))))
+
+(defun* docker-robe::select-volume-local-path (container &key (use-cache nil))
+  (or (when use-cache (docker-robe::project-cached-value :volume-local-path-alist))
+      (let* ((cmd (format "inspect --format='{{range $conf := .Mounts}}{{$conf.Destination}}:{{end}}' %s" container)))
+        (loop for remote-path in (split-string (docker-robe::exec 'shell-command-to-string cmd) ":")
+              for local-path = (when (and (string-match "/" remote-path)
+                                          (y-or-n-p (format "Local source is mounted on %s" remote-path)))
+                                 (directory-file-name (read-directory-name "Local source path : " nil nil t default-directory)))
+              if local-path
+              collect (cons remote-path local-path)))))
 
 (defun docker-robe::ensure-library (container)
   (let ((robe-library-dir (shell-quote-argument (file-name-directory robe-ruby-path))))
     (docker-robe::exec 'shell-command (format "exec -i %s mkdir -p %s" container robe-library-dir))
     (docker-robe::exec 'shell-command (format "cp %s %s:%s" (shell-quote-argument robe-ruby-path) container robe-library-dir))))
 
-(defun docker-robe::local-path-mapped (remote-path)
-  (loop for (remote-map . local-map) in docker-robe:source-map
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Handling robe response
+
+(defun docker-robe::filter-response (endpoint res)
+  (cond
+    ((string= endpoint "class_locations")
+     (mapcar 'docker-robe::local-path-for res))
+    ((string= endpoint "method_targets")
+     (mapcar
+      (lambda (method-definition)
+        (multiple-value-bind (classnm flg methodnm args path row-index) method-definition
+          `(,classnm ,flg ,methodnm ,args ,(docker-robe::local-path-for path) ,row-index)))
+      res))
+    (t
+     res)))
+
+(defun docker-robe::local-path-for (remote-path)
+  (loop for (remote-map . local-map) in (buffer-local-value 'docker-robe:volume-local-path-alist (inf-ruby-buffer))
         for re = (rx-to-string `(and bos ,remote-map))
         for local-path = (replace-regexp-in-string re local-map remote-path)
         if (not (string= remote-path local-path))
         return local-path
         finally return remote-path))
 
-(defun docker-robe::filter-response (endpoint res)
-  (cond
-    ((string= endpoint "class_locations")
-     (let ((docker-robe:source-map (buffer-local-value 'docker-robe:source-map (inf-ruby-buffer))))
-       (mapcar 'docker-robe::local-path-mapped res)))
-    ((string= endpoint "method_targets")
-     (let ((docker-robe:source-map (buffer-local-value 'docker-robe:source-map (inf-ruby-buffer))))
-       (mapcar
-        (lambda (method-definition)
-          (multiple-value-bind (classnm flg methodnm args path row-index) method-definition
-            `(,classnm ,flg ,methodnm ,args ,(docker-robe::local-path-mapped path) ,row-index)))
-        res)))
-    (t
-     res)))
 
+;;;;;;;;;;;;
+;; Advice
+
+(defadvice robe-start (around docker-robe:dockerize disable)
+  (cond
+   ((and robe-running
+         (not (ad-get-arg 0))
+         (get-buffer-process (inf-ruby-buffer)))
+    ad-do-it)
+   (t
+    (let* ((docker-robe:enabled t)
+           (docker-robe:project-root (when (functionp docker-robe:project-root-detect-function)
+                                       (funcall docker-robe:project-root-detect-function)))
+           (docker-robe:container (docker-robe::select-container :use-cache t))
+           (robe-host nil)
+           (robe-port (docker-robe::select-port docker-robe:container :use-cache t))
+           (volume-local-path-alist (docker-robe::select-volume-local-path docker-robe:container :use-cache t)))
+      (docker-robe::store-project-cache :container docker-robe:container
+                                        :port robe-port
+                                        :volume-local-path-alist volume-local-path-alist)
+      (docker-robe::ensure-library docker-robe:container)
+      ad-do-it
+      (with-current-buffer (inf-ruby-buffer)
+        (set (make-local-variable 'docker-robe:container) docker-robe:container)
+        (set (make-local-variable 'robe-port) robe-port)
+        (set (make-local-variable 'docker-robe:volume-local-path-alist) volume-local-path-alist))))))
+
+(defadvice robe-request (around docker-robe:dockerize disable)
+  (let ((robe-host "127.0.0.1")
+        (robe-port (buffer-local-value 'robe-port (inf-ruby-buffer))))
+    ad-do-it
+    (setq ad-return-value (docker-robe::filter-response (ad-get-arg 0) ad-return-value))))
+
+(defadvice inf-ruby-console-run (around docker-robe:dockerize disable)
+  (when (and docker-robe:enabled
+             docker-robe:container)
+    (ad-set-arg 0 (format "docker exec -it %s %s" docker-robe:container (ad-get-arg 0))))
+  ad-do-it)
+
+
+;;;;;;;;;;;;;;;;;;;
+;; User Function
 
 ;;;###autoload
 (defun docker-robe:activate ()
@@ -166,36 +279,19 @@
   (ad-disable-regexp "docker-robe:dockerize")
   (ad-deactivate-regexp "docker-robe:dockerize"))
 
-
-(defadvice robe-start (around docker-robe:dockerize disable)
-  (cond
-   ((and robe-running
-         (not (ad-get-arg 0))
-         (get-buffer-process (inf-ruby-buffer)))
-    ad-do-it)
-   (t
-    (let* ((docker-robe:enabled t)
-           (docker-robe:container (docker-robe::select-container))
-           (robe-host nil)
-           (robe-port (docker-robe::select-port docker-robe:container))
-           (source-map (docker-robe::source-map docker-robe:container)))
-      (docker-robe::ensure-library docker-robe:container)
-      ad-do-it
-      (with-current-buffer (inf-ruby-buffer)
-        (set (make-local-variable 'robe-port) robe-port)
-        (set (make-local-variable 'docker-robe:source-map) source-map))))))
-
-(defadvice robe-request (around docker-robe:dockerize disable)
-  (let ((robe-host "127.0.0.1")
-        (robe-port (buffer-local-value 'robe-port (inf-ruby-buffer))))
-    ad-do-it
-    (setq ad-return-value (docker-robe::filter-response (ad-get-arg 0) ad-return-value))))
-
-(defadvice inf-ruby-console-run (around docker-robe:dockerize disable)
-  (when (and docker-robe:enabled
-             docker-robe:container)
-    (ad-set-arg 0 (format "docker exec -it %s %s" docker-robe:container (ad-get-arg 0))))
-  ad-do-it)
+;;;###autoload
+(defun docker-robe:configure-current-project ()
+  "Configure project has `current-buffer'."
+  (interactive)
+  (let* ((docker-robe:project-root (or (when (functionp docker-robe:project-root-detect-function)
+                                         (funcall docker-robe:project-root-detect-function))
+                                       (error "Can't detect project root path for %s" (buffer-file-name))))
+         (container (docker-robe::select-container))
+         (port (docker-robe::select-port container))
+         (volume-local-path-alist (docker-robe::select-volume-local-path container)))
+    (docker-robe::store-project-cache :container container
+                                      :port port
+                                      :volume-local-path-alist volume-local-path-alist)))
 
 
 (provide 'docker-robe)
